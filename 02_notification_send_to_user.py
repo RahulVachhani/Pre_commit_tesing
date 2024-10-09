c03575df3c115675a4f3e9c7cacf2e56a3b7d52e
Rahul <rahul.vachhani@openxcell.com>
2024-10-09 09:17:13 +0530

    pre-commit tesing

---
+++ 02_notification_send_to_user.py
@@ -0,0 +1,26 @@
+# from terminal we use this two different commands
+
+# 1) zenity --info --text="This is a notification message"
+# 2) notify-send "Title" "This is the notification message"
+
+
+# This for like an alert in js
+# import subprocess
+
+# def send_notification(title, message):
+#     subprocess.run(['notify-send', title, message])
+
+# # Example usage
+# send_notification("Good Morning ", "Time to drink water")
+
+
+# Another like promt
+# import os
+# import time
+
+# command = '''zenity --entry --text="Please enter your name:"'''
+# command1= f'''zenity --info --text=" Welcome {command}:"'''
+# os.system(command)
+# os.system
+
+123456789101112131415161718192021222324252627282930313233343536373839404142434445464748495051525354555657585960616263646566676869707172737475767778798081828384858687888990919293949596979899100101102103104105106107108109110111112113114115116117118119120121122123124125126127128129130131132133134135136137138139140141142143144145146147148149150151152153154155156157158159160161162163164165166167168169170171172173174175176177178179180181182183184185186187188189190191192193194195196197198199
